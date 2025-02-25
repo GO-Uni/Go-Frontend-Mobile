dec1a1c2687cbeca0cbe946b52aa541c363f06cc
hananeltassa <hananeltassa15@gmail.com>
2025-02-25 17:53:57 +0200

    Added centralized text style

---
+++ lib/theme/text_styles.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'colors.dart';
+
+class AppTextStyles {
+  static const TextStyle bodyLarge = TextStyle(
+    fontSize: 18,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600, // SemiBold
+    color: AppColors.darkGray,
+  );
+
+  static const TextStyle bodyMedium = TextStyle(
+    fontSize: 16,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500, // Medium
+    color: AppColors.mediumGray,
+  );
+
+  static const TextStyle bodySmall = TextStyle(
+    fontSize: 14,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400, // Light
+    color: AppColors.mediumGray,
+  );
+
+  static const TextStyle primaryButton = TextStyle(
+    fontSize: 16,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600, // SemiBold
+    color: Colors.white,
+  );
+
+  static const TextStyle highlightText = TextStyle(
+    fontSize: 14,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.bold,
+    color: AppColors.primary,
+  );
+}
