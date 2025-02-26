06f5237999f91c0155c9d1d354535fd97bff3afa
hananeltassa <hananeltassa15@gmail.com>
2025-02-26 13:31:22 +0200

    Updated routes

--- lib/services/routes.dart
+++ lib/services/routes.dart
@@ -2,11 +2,19 @@
 import 'package:go_router/go_router.dart';
 import '../screens/sign_up_options.dart';
 import '../screens/sign_up_business.dart';
+import '../screens/log_in.dart';
 
 final GoRouter appRouter = GoRouter(
   initialLocation: '/',
   routes: [
-    GoRoute(path: '/1', builder: (context, state) => SignUpOptions()),
-    GoRoute(path: '/', builder: (context, state) => SignUpBusiness()),
+    GoRoute(
+      path: '/sign-up-options',
+      builder: (context, state) => SignUpOptions(),
+    ),
+    GoRoute(
+      path: '/sign-up-business',
+      builder: (context, state) => SignUpBusiness(),
+    ),
+    GoRoute(path: '/', builder: (context, state) => Login()),
   ],
 );
