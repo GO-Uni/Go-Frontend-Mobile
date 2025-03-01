a1022137c0f8711fdcccf7db4647f0785d77b7a1
hananeltassa <hananeltassa15@gmail.com>
2025-03-02 01:52:11 +0200

    Updated WhereToNextScreen

--- lib/screens/where_to_next_screen.dart
+++ lib/screens/where_to_next_screen.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 import '../theme/text_styles.dart';
 import '../theme/colors.dart';
+import '../widgets/category_card.dart';
 
 class WhereToNextScreen extends StatelessWidget {
   const WhereToNextScreen({super.key});
+
+  final List<Map<String, String>> categories = const [
+    {"title": "Historical Landmarks", "image": "assets/images/historical.png"},
+    {"title": "Natural Wonders", "image": "assets/images/natural.jpg"},
+    {"title": "Sports", "image": "assets/images/sports.jpg"},
+    {"title": "Historical Landmarks", "image": "assets/images/historical.png"},
+    {"title": "Natural Wonders", "image": "assets/images/natural.jpg"},
+    {"title": "Sports", "image": "assets/images/sports.jpg"},
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -13,11 +23,31 @@ class WhereToNextScreen extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
-            padding: EdgeInsets.all(16),
+            padding: EdgeInsets.only(top: 10, left: 12, right: 12),
             child: Text(
               "Where to next?",
               style: AppTextStyles.headingLarge.copyWith(
                 color: AppColors.darkGray,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12.0),
+              child: GridView.builder(
+                itemCount: categories.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 20,
+                  mainAxisSpacing: 2,
+                  childAspectRatio: 1.0,
+                ),
+                itemBuilder: (context, index) {
+                  return CategoryCard(
+                    title: categories[index]['title']!,
+                    imagePath: categories[index]['image']!,
+                  );
+                },
               ),
             ),
           ),
