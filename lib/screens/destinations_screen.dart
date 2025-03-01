6fd2249a449ba922711be9b831b3c428f6700e39
hananeltassa <hananeltassa15@gmail.com>
2025-03-02 00:22:01 +0200

    Added app_layout widget

--- lib/screens/destinations_screen.dart
+++ lib/screens/destinations_screen.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MaterialApp(home: DestinationsScreen()));
+}
+
 class DestinationsScreen extends StatelessWidget {
   const DestinationsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Explore amazing destinations!")));
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      body: Center(
+        child: Text(
+          "Explore amazing destinations!",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/app_layout.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../services/routes.dart';
+import 'header.dart';
+import 'bottom_nav_bar.dart';
+
+class AppLayout extends StatefulWidget {
+  final Widget child;
+  const AppLayout({super.key, required this.child});
+
+  @override
+  State<AppLayout> createState() => _AppLayoutState();
+}
+
+class _AppLayoutState extends State<AppLayout> {
+  Widget? _selectedHeaderContent;
+  Key _headerKey = UniqueKey();
+
+  void _onHeaderTabSelected(int index) {
+    switch (index) {
+      case 0:
+        context.go(ConfigRoutes.destinations);
+        break;
+      case 1:
+        context.go(ConfigRoutes.saved);
+        break;
+      default:
+        _selectedHeaderContent = null;
+    }
+  }
+
+  void _resetHeader() {
+    setState(() {
+      _headerKey = UniqueKey();
+      _selectedHeaderContent = null;
+    });
+  }
+
+  int _getBottomNavIndex(BuildContext context) {
+    final location = GoRouterState.of(context).uri.toString();
+    if (location == ConfigRoutes.chatbot) return 1;
+    if (location == ConfigRoutes.whereToNext) return 2;
+    if (location == ConfigRoutes.bookings) return 3;
+    if (location == ConfigRoutes.profile) return 4;
+    return 0;
+  }
+
+  void _onBottomNavTap(int index) {
+    _resetHeader();
+    switch (index) {
+      case 0:
+        context.go(ConfigRoutes.maps);
+        break;
+      case 1:
+        context.go(ConfigRoutes.chatbot);
+        break;
+      case 2:
+        context.go(ConfigRoutes.whereToNext);
+        break;
+      case 3:
+        context.go(ConfigRoutes.bookings);
+        break;
+      case 4:
+        context.go(ConfigRoutes.profile);
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    int selectedIndex = _getBottomNavIndex(context);
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Header(key: _headerKey, onTabSelected: _onHeaderTabSelected),
+          Expanded(child: _selectedHeaderContent ?? widget.child),
+        ],
+      ),
+      bottomNavigationBar: BottomNavBar(
+        selectedIndex: selectedIndex,
+        onTap: _onBottomNavTap,
+      ),
+    );
+  }
+}
