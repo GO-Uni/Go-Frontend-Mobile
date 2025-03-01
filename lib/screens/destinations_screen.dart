924800a7c53b75b993c626bcca93bdf4a666d4d0
hananeltassa <hananeltassa15@gmail.com>
2025-03-01 18:50:41 +0200

    Added placeholders for screens

--- lib/screens/destinations_screen.dart
+++ lib/screens/destinations_screen.dart
@@ -5,6 +5,6 @@ class DestinationsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(body: Center(child: Text("Explore amazing destinations!")));
   }
 }
