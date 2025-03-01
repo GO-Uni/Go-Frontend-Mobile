38a065ba4a41a416d475c8c68ab25620028fd727
hananeltassa <hananeltassa15@gmail.com>
2025-03-01 19:53:07 +0200

    Updated Header Widget

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -2,21 +2,45 @@ import 'package:flutter/material.dart';
 import '../theme/text_styles.dart';
 import '../theme/colors.dart';
 
-class Header extends StatelessWidget {
-  final TabController tabController;
+class Header extends StatefulWidget {
+  final Function(int) onTabSelected;
 
-  const Header({super.key, required this.tabController});
+  const Header({super.key, required this.onTabSelected});
+
+  @override
+  State<Header> createState() => _HeaderState();
+}
+
+class _HeaderState extends State<Header> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  int? _selectedIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+    _selectedIndex = null;
+
+    _tabController.addListener(() {
+      if (_tabController.indexIsChanging) {
+        setState(() {
+          _selectedIndex = _tabController.index;
+        });
+        widget.onTabSelected(_tabController.index);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: const BorderRadius.only(
-          bottomLeft: Radius.circular(15),
-          bottomRight: Radius.circular(15),
-        ),
-      ),
+      color: Colors.white,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -32,7 +56,7 @@ class Header extends StatelessWidget {
                     color: AppColors.primary,
                   ),
                 ),
-                const SizedBox(width: 4),
+                const SizedBox(width: 10),
                 Expanded(
                   child: Container(
                     padding: const EdgeInsets.symmetric(horizontal: 12),
@@ -73,11 +97,23 @@ class Header extends StatelessWidget {
             child: SizedBox(
               width: 250,
               child: TabBar(
-                controller: tabController,
-                indicator: UnderlineTabIndicator(
-                  borderSide: BorderSide(width: 2, color: AppColors.primary),
-                  insets: EdgeInsets.symmetric(horizontal: 120),
-                ),
+                controller: _tabController,
+                onTap: (index) {
+                  setState(() {
+                    _selectedIndex = index;
+                  });
+                  widget.onTabSelected(index);
+                },
+                indicator:
+                    _selectedIndex == null
+                        ? const BoxDecoration()
+                        : UnderlineTabIndicator(
+                          borderSide: BorderSide(
+                            width: 2,
+                            color: AppColors.primary,
+                          ),
+                          insets: EdgeInsets.symmetric(horizontal: 120),
+                        ),
                 indicatorSize: TabBarIndicatorSize.tab,
                 labelStyle: AppTextStyles.bodyMedium.copyWith(fontSize: 14),
                 unselectedLabelStyle: AppTextStyles.bodyMedium.copyWith(
