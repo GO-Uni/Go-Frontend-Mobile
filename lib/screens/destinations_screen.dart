9d51f1f11f8298531a274f4d19a23ff5b2374296
hananeltassa <hananeltassa15@gmail.com>
2025-03-04 00:38:36 +0200

    Updated DestinationsScreen, Added dest cards

--- lib/screens/destinations_screen.dart
+++ lib/screens/destinations_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../theme/text_styles.dart';
 import '../theme/colors.dart';
+import '../widgets/destination_card.dart';
 
 class DestinationsScreen extends StatelessWidget {
   const DestinationsScreen({super.key});
@@ -14,27 +15,74 @@ class DestinationsScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
-              padding: const EdgeInsets.all(16),
+              padding: const EdgeInsets.only(top: 16, left: 16),
               child: Text(
                 "Recommended",
                 style: AppTextStyles.bodyLarge.copyWith(fontSize: 24),
               ),
             ),
-            SizedBox(
-              height: 200,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 5,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+
+              child: GridView.builder(
+                padding: EdgeInsets.only(top: 14, left: 13, right: 13),
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 14,
+                  mainAxisSpacing: 25,
+                  childAspectRatio: 0.9,
+                ),
+                itemCount: 2, //destinations.length
                 itemBuilder: (context, index) {
-                  return;
+                  return DestinationCard(
+                    imageUrl:
+                        "https://images.unsplash.com/photo-1726064855757-ac8720008fe0?q=80",
+                    name: "Ancient Castle",
+                    description: "A historical place",
+                    rating: 3,
+                  );
                 },
               ),
             ),
+
             Padding(
-              padding: const EdgeInsets.all(16),
+              padding: const EdgeInsets.only(top: 16, left: 16),
               child: Text(
                 "All Destinations",
                 style: AppTextStyles.bodyLarge.copyWith(fontSize: 24),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+
+              child: GridView.builder(
+                padding: EdgeInsets.only(top: 14, left: 13, right: 13),
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 14,
+                  mainAxisSpacing: 25,
+                  childAspectRatio: 0.9,
+                ),
+                itemCount: 4, //destinations.length
+                itemBuilder: (context, index) {
+                  return DestinationCard(
+                    imageUrl:
+                        "https://images.unsplash.com/photo-1726064855757-ac8720008fe0?q=80",
+                    name: "Ancient Castle",
+                    description: "A historical place",
+                    rating: 3,
+
+                    // imageUrl: destinations[index].imageUrl,
+                    // name: destinations[index].name,
+                    // description: destinations[index].description,
+                    // rating: destinations[index].rating,
+                  );
+                },
               ),
             ),
           ],
