c423e10c444e5e9abef83a1631ed75fbfa1cd91c
hananeltassa <hananeltassa15@gmail.com>
2025-03-05 01:37:20 +0200

    Updated ChatbotScreen TextField

--- lib/screens/chatbot_screen.dart
+++ lib/screens/chatbot_screen.dart
@@ -12,32 +12,50 @@ class ChatbotScreen extends StatelessWidget {
       body: Column(
         children: [
           Expanded(child: Container()),
-          Container(
-            height: 55,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: AppColors.mediumGray,
-            ),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: "Type here...",
-                      border: InputBorder.none,
-                      hintStyle: AppTextStyles.bodyRegular.copyWith(
-                        color: AppColors.lightGray,
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+            child: Container(
+              height: 55,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: AppColors.mediumGray,
+              ),
+              child: Row(
+                children: [
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Type here...",
+                        border: InputBorder.none,
+                        hintStyle: AppTextStyles.bodyRegular.copyWith(
+                          color: AppColors.lightGray,
+                        ),
                       ),
-                      contentPadding: const EdgeInsets.symmetric(
-                        horizontal: 16,
-                      ),
+                      style: const TextStyle(color: Colors.white),
                     ),
-                    style: TextStyle(color: Colors.white),
                   ),
-                ),
-              ],
+                  Container(
+                    decoration: BoxDecoration(
+                      color: AppColors.darkGray,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.send,
+                        color: Colors.white,
+                        size: 26,
+                      ),
+                      padding: const EdgeInsets.all(16),
+                      constraints: const BoxConstraints(),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
+          const SizedBox(height: 26),
         ],
       ),
     );
