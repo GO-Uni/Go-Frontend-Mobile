7987839cb46104df8ce184d65e11d1599f0c415f
hananeltassa <hananeltassa15@gmail.com>
2025-03-06 03:57:31 +0200

    Updated ProfileScreen Added UserType

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import '../theme/colors.dart';
+import '../widgets/profile_header.dart';
+
+enum UserType { business, normal }
+
+class UserModel {
+  final String name;
+  final String email;
+  final UserType userType;
+
+  const UserModel({
+    required this.name,
+    required this.email,
+    required this.userType,
+  });
+}
 
 class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
+  final UserModel user;
+
+  const ProfileScreen({super.key, required this.user});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColors.lightGreen,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(14),
+              child: ProfileHeader(user: user),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
+final UserModel defaultUser = UserModel(
+  name: "Hanan",
+  email: "hanan@email.com",
+  userType: UserType.normal,
+);
