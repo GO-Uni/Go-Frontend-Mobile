20d37e11e0b788f2f83bcfd5d023e24779593e48
hananeltassa <hananeltassa15@gmail.com>
2025-03-08 00:35:05 +0200

    Added Images in DetailedDestinationScreen

--- lib/screens/detailed_destination_screen.dart
+++ lib/screens/detailed_destination_screen.dart
@@ -3,13 +3,46 @@ import 'package:go_router/go_router.dart';
 import '../theme/text_styles.dart';
 import '../theme/colors.dart';
 
-class DetailedDestinationScreen extends StatelessWidget {
+class DetailedDestinationScreen extends StatefulWidget {
   const DetailedDestinationScreen({super.key});
+
+  @override
+  State<DetailedDestinationScreen> createState() =>
+      _DetailedDestinationScreenState();
+}
+
+class _DetailedDestinationScreenState extends State<DetailedDestinationScreen> {
+  late String selectedImage;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    final extra = GoRouterState.of(context).extra;
+    final destination = (extra is Map<String, dynamic>) ? extra : {};
+
+    List<String> images =
+        destination["images"] ??
+        [
+          destination["imageUrl"] ??
+              "https://images.pexels.com/photos/2990603/pexels-photo-2990603.jpeg?auto=compress&cs=tinysrgb&w=600",
+        ];
+
+    selectedImage = images.first;
+  }
 
   @override
   Widget build(BuildContext context) {
     final extra = GoRouterState.of(context).extra;
     final destination = (extra is Map<String, dynamic>) ? extra : {};
+
+    List<String> images =
+        destination["images"] ??
+        [
+          destination["imageUrl"] ??
+              "https://images.pexels.com/photos/2990603/pexels-photo-2990603.jpeg?auto=compress&cs=tinysrgb&w=600",
+          "https://images.pexels.com/photos/2990603/pexels-photo-2990603.jpeg?auto=compress&cs=tinysrgb&w=600",
+        ];
 
     return Scaffold(
       backgroundColor: AppColors.lightGreen,
@@ -17,6 +50,7 @@ class DetailedDestinationScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            // Header Section
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
               child: Row(
@@ -51,6 +85,66 @@ class DetailedDestinationScreen extends StatelessWidget {
                     size: 30,
                   ),
                 ],
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(16),
+                child: Image.network(
+                  selectedImage,
+                  width: double.infinity,
+                  height: 250,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+              child: SizedBox(
+                height: 60,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: images.length,
+                  itemBuilder: (context, index) {
+                    bool isSelected = selectedImage == images[index];
+
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedImage = images[index];
+                        });
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 10),
+                        child: Stack(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(16),
+                              child: Image.network(
+                                images[index],
+                                width: 60,
+                                height: 60,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            if (isSelected)
+                              Container(
+                                width: 60,
+                                height: 60,
+                                decoration: BoxDecoration(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  borderRadius: BorderRadius.circular(16),
+                                ),
+                              ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
               ),
             ),
           ],
