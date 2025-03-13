ae376c507ecfca587866c9bc7d99a03127d6866d
hananeltassa <hananeltassa15@gmail.com>
2025-03-13 22:02:44 +0200

    Updated loginService role_id

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -15,6 +15,9 @@ class AuthProvider extends ChangeNotifier {
   String? _errorMessage;
   String? get errorMessage => _errorMessage;
 
+  int? _roleId;
+  int? get roleId => _roleId;
+
   Future<bool> registerUser({
     required String name,
     required String email,
@@ -65,6 +68,9 @@ class AuthProvider extends ChangeNotifier {
     }
 
     final data = response['data']?['data'];
+    final user = data['user'];
+    final token = data['token'];
+
     if (data == null ||
         !data.containsKey('user') ||
         !data.containsKey('token')) {
@@ -73,12 +79,13 @@ class AuthProvider extends ChangeNotifier {
       return false;
     }
 
-    final user = data['user'];
-    final token = data['token'];
+    _roleId = user['role_id'];
+    log("User Role ID: $_roleId");
     log("Extracted User: $user");
     log("Extracted Token: $token");
 
     await _secureStorage.write(key: 'auth_token', value: token);
+    await _secureStorage.write(key: 'role_id', value: _roleId.toString());
 
     notifyListeners();
     return true;
@@ -88,8 +95,19 @@ class AuthProvider extends ChangeNotifier {
     return await _secureStorage.read(key: 'auth_token');
   }
 
+  Future<int?> getRoleId() async {
+    String? roleIdStr = await _secureStorage.read(key: 'role_id');
+    if (roleIdStr != null) {
+      _roleId = int.tryParse(roleIdStr);
+      notifyListeners();
+      return _roleId;
+    }
+    return null;
+  }
+
   Future<void> logoutUser() async {
     await _secureStorage.delete(key: 'auth_token');
+    await _secureStorage.delete(key: 'role_id');
     notifyListeners();
   }
 }
