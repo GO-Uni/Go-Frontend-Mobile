5665c8eaf451908b125ac9204269a77cce3a924d
hananeltassa <hananeltassa15@gmail.com>
2025-03-16 00:10:56 +0200

    Updated ProfileService authToken

--- lib/services/profile_service.dart
+++ lib/services/profile_service.dart
@@ -1,19 +1,36 @@
 import 'dart:developer';
 import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:go_frontend_mobile/services/api_routes.dart';
 import 'package:go_frontend_mobile/services/dio_client.dart';
 
 class ProfileService {
   final DioClient _dioClient;
+  final FlutterSecureStorage _secureStorage = const FlutterSecureStorage();
 
   ProfileService(this._dioClient);
 
   Future<bool> updateProfile(Map<String, dynamic> profileData) async {
     try {
+      String? token = await _secureStorage.read(key: 'auth_token');
+
+      if (token == null) {
+        log("No auth token found. User is not authenticated.");
+        return false;
+      }
+
       Response response = await _dioClient.dio.put(
         ApiRoutes.updateProfile,
         data: profileData,
+        options: Options(
+          headers: {
+            "Authorization": "Bearer $token",
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+          },
+        ),
       );
+
       if (response.statusCode == 200) {
         log("Profile updated successfully");
         return true;
