7123950c21ab842fae86037f38b9173c91d2692e
hananeltassa <hananeltassa15@gmail.com>
2025-03-18 23:03:42 +0200

    Added DestinationService

--- lib/services/api_routes.dart
+++ lib/services/api_routes.dart
@@ -3,4 +3,8 @@ final class ApiRoutes {
   static const String register = "/register";
   static const String categories = "/categories";
   static const String updateProfile = "/profile/edit";
+
+  static String categoryDestinations(String category) {
+    return "/destinations/category/$category";
+  }
 }
---
+++ lib/services/destination_service.dart
@@ -0,0 +1,30 @@
+import 'package:dio/dio.dart';
+import 'dio_client.dart';
+import 'api_routes.dart';
+import 'dart:developer';
+
+class DestinationService {
+  final DioClient _dioClient;
+
+  DestinationService(this._dioClient);
+
+  Future<List<Map<String, dynamic>>> fetchDestinationsByCategory(
+    String category,
+  ) async {
+    try {
+      Response res = await _dioClient.dio.get(
+        ApiRoutes.categoryDestinations(category),
+      );
+
+      if (res.data["status"] == "success") {
+        return List<Map<String, dynamic>>.from(res.data["data"]);
+      } else {
+        log("❌ API returned an error: ${res.data}");
+        return [];
+      }
+    } on DioException catch (e) {
+      log("❌ Error fetching destinations: ${e.message}");
+      return [];
+    }
+  }
+}
