9220c8608c88a28f48f18bf3239ccc0af2337a4c
hananeltassa <hananeltassa15@gmail.com>
2025-04-13 23:45:02 +0300

    Updated Maps to show Destinations pin points

--- lib/screens/maps_screen.dart
+++ lib/screens/maps_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:go_frontend_mobile/providers/destination_provider.dart';
 
 class MapsScreen extends StatefulWidget {
   const MapsScreen({super.key});
@@ -10,28 +13,58 @@ class MapsScreen extends StatefulWidget {
 
 class _MapsScreenState extends State<MapsScreen> {
   late GoogleMapController mapController;
+  final LatLng _defaultCenter = const LatLng(33.8547, 35.8623);
 
-  final LatLng _center = const LatLng(33.8547, 35.8623);
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Provider.of<DestinationProvider>(
+        context,
+        listen: false,
+      ).fetchAllDestinations();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    final destinationProvider = Provider.of<DestinationProvider>(context);
+    final Set<Marker> markers = {};
+
+    for (var destination in destinationProvider.destinations) {
+      final latStr = destination["latitude"];
+      final lngStr = destination["longitude"];
+
+      if (latStr != null && lngStr != null) {
+        final double? lat = double.tryParse(latStr.toString());
+        final double? lng = double.tryParse(lngStr.toString());
+
+        if (lat != null && lng != null) {
+          markers.add(
+            Marker(
+              markerId: MarkerId(destination["id"].toString()),
+              position: LatLng(lat, lng),
+              infoWindow: InfoWindow(
+                title: destination["business_name"] ?? "Unknown",
+                snippet: destination["category_name"] ?? "No category info",
+              ),
+              onTap: () {
+                log("Marker tapped: ${destination["business_name"]}");
+              },
+            ),
+          );
+        }
+      }
+    }
+
     return Scaffold(
       body: GoogleMap(
         onMapCreated: (controller) {
           mapController = controller;
         },
-        onTap: (argument) {
-          //print(argument.latitude);
-        },
-        initialCameraPosition: CameraPosition(target: _center, zoom: 11),
-        markers: {
-          Marker(
-            markerId: MarkerId("1"),
-            position: _center,
-            infoWindow: InfoWindow(title: 'hanan', snippet: 'hello hanan'),
-            onTap: () => {},
-          ),
-        },
+        initialCameraPosition: CameraPosition(target: _defaultCenter, zoom: 11),
+        markers: markers,
       ),
     );
   }
